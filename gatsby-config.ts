cf5d4b11fc98fcde38485ceb573370479412a251
LeoBang <bluke8489@gmail.com>
2023-01-19 17:47:39 +0900

    [Config] transform 의존성 및  EmotionJS 플러그인 설치 - gatsby-config.ts에 플러그인 적용.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,6 +18,9 @@ const config: GatsbyConfig = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    /**
+     * Typescript Config
+     */
     {
       resolve: 'gatsby-plugin-typescript',
       options: {
@@ -25,16 +28,59 @@ const config: GatsbyConfig = {
         allExtensions: true,
       },
     },
+    /**
+     * Source plugin Config
+     */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `documents`,
+        path: `${__dirname}/documents`,
+      },
+    },
+    /**
+     * Gatsby Transformer Configs
+     */
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-autolink-headers`,
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 768,
+              quality: 100,
+              withWebp: true,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-smartypants',
+            options: {
+              dashes: 'oldschool',
+            },
+          },
+        ],
+      },
+    },
+
+    /**
+     * Gatsby Image Configs
+     */
     `gatsby-plugin-image`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    /**
+     * Gatsby Style Configs
+     */
+    `gatsby-plugin-emotion`,
   ],
 }
 
