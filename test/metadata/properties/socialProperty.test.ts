65c6e1f147d6f3d9bc0f4e1c4e9aff8c69238221
LeoBang <bluke8489@gmail.com>
2023-01-23 12:31:51 +0900

    [Test] datalayer Wrapper 타입 테스트 (#8) - urlExtractor - emailproperty - socialproperty - stringproperty

---
+++ test/metadata/properties/socialProperty.test.ts
@@ -0,0 +1,55 @@
+import WebLinkProperty from '../../../src/utils/metadata/properties/WebLiinkProperty'
+
+test('isValid - 실패 (지원하지 않는 social)', () => {
+  const url = 'twitterr.com/Arsenal'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(false)
+})
+
+test('isValid - 실패 (path variable 없음)', () => {
+  const url = 'https://twitter.com/'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(false)
+})
+
+test('isValid - 실패 (url-unsafe 문자)', () => {
+  const url = 'https://twitter.com/id.fef>@@e'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(false)
+})
+
+test('isValid - 성공 (twitter)', () => {
+  const url = 'https://twitter.com/Arsenal'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
+
+test('isValid - 성공 (linkedin)', () => {
+  const url = 'https://www.linkedin.com/in/seok-jin-bang-877882249/'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
+
+test('isValid - 성공 (facebook)', () => {
+  const url = 'https://www.facebook.com/arsenal'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
+
+test('isValid - 성공 (instagram)', () => {
+  const url = 'https://www.instagram.com/ba1vg/'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
+
+test('isValid - 성공 (github)', () => {
+  const url = 'https://github.com/leobang17'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
+
+test('isValid - 성공 (https 없이)', () => {
+  const url = 'github.com/leobang17'
+  const social = new WebLinkProperty(url)
+  expect(social.isValid()).toBe(true)
+})
---
+++ test/metadata/urlExctractor.test.ts
@@ -0,0 +1,54 @@
+import * as module from '../../src/utils/metadata/urlExtractor'
+import { socialProviders } from '../../src/@types/social-provider-types'
+
+// extractDomain Test
+test('extractDomain - 실패 (빈 url)', () => {
+  const url = ''
+  expect(() => {
+    module.extractDomain(url)
+  }).toThrow()
+})
+
+test('extractDomain - 실패 (잘못된 형태1)', () => {
+  const url = 'fefessefe.com'
+  expect(() => {
+    module.extractDomain(url)
+  }).toThrow()
+})
+
+test('extractDomain - 실패 (잘못된 형태2)', () => {
+  const url = 'https:/fefessefe.com/'
+  expect(() => {
+    module.extractDomain(url)
+  }).toThrow()
+})
+
+test('extractDomain - 성공 (https:// x)', () => {
+  const domain = 'linkedin'
+  const url = `${domain}.com/leobang`
+  const res = module.extractDomain(url)
+  expect(res).toBe(domain)
+})
+
+test('extractDomain - 성공 (https://)', () => {
+  const domain = 'twitter'
+  const url = `https://${domain}.com/leobang`
+  const res = module.extractDomain(url)
+  expect(res).toBe(domain)
+})
+
+// extractSocialProvider Test
+test('extractSocialProvider - 실패 (extractDomain undefined 반환', () => {
+  jest.spyOn(module, 'extractDomain').mockImplementation(() => undefined)
+  expect(() => {
+    module.extractSocialProvider('anystring')
+  }).toThrow()
+})
+
+socialProviders.forEach(social => {
+  test(`extractSocialProvider - 성공 (${social})`, () => {
+    jest.spyOn(module, 'extractDomain').mockImplementation(() => social)
+    const res = module.extractSocialProvider('anyString')
+    expect(res).toBe(social)
+  })
+})
