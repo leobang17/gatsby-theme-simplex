4888e5f7a301ff4d0a10dfba74f0104446aa21d9
LeoBang <bluke8489@gmail.com>
2023-02-21 13:42:59 +0900

    [Feat] Footer 생성 (#20)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+/** @jsx jsx */
 
-const Footer: React.FC = () => {
-  return <div>Footer</div>
+import { css, jsx } from '@emotion/react'
+import { FC } from 'react'
+import { BORDER_MUSK } from 'styles/Color'
+import FooterOrigin from './cells/FooterOrigin'
+import FooterUsername from './cells/FooterUsername'
+
+type FooterProps = {}
+
+const style = css`
+  width: 100vw;
+  margin-top: 5rem;
+  padding: 1rem 0 1rem 0;
+  border-top: 1px solid ${BORDER_MUSK};
+  font-size: small;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #515055;
+
+  a {
+    transition: color 0.15s ease;
+    :hover {
+      color: skyblue;
+    }
+  }
+`
+
+const Footer: FC<FooterProps> = ({}) => {
+  return (
+    <div css={style}>
+      <p>
+        <FooterUsername />
+        <span>Theme by </span>
+        <FooterOrigin />
+        <span>, Built with Gatsby</span>
+      </p>
+    </div>
+  )
 }
 
 export default Footer
