e92e4d0c8b736e610b1203fd732fa69c07b3d1cb
LeoBang <bluke8489@gmail.com>
2023-02-25 01:25:12 +0900

    [Chore] 이용하지 않는 util function 및 테스트 삭제 (#28)

--- test/slug.test.ts
+++
@@ -1,8 +0,0 @@
-import { slugifyFilepath } from 'utils/slug'
-
-test('slugifyFilepath', () => {
-  const filepath = '/Computor Science/운영체제/공룡책 운영체제/'
-  const res = slugifyFilepath(filepath)
-
-  expect(res).toBe('/Computor-Science/운영체제/공룡책-운영체제')
-})
