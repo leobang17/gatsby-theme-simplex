b488a96a1e953320a99583c0cfcb5afbc83a574f
LeoBang <bluke8489@gmail.com>
2023-03-26 01:44:14 +0900

    [Feat] 검색 엔진 최적화 (#45) - Gatsby Head API를 이용해 CategoryPage, ArticlePage를 위한 Head 선언 및 적용 - Seo 컴포넌트 정의   - title, open graph, canonical 등

---
+++ src/components/head.tsx
@@ -0,0 +1,29 @@
+import CategoryStrings from 'datastructures/category/CategoryStrings'
+import { HeadProps } from 'gatsby'
+import { CategoryPageContext } from 'templates/CategoryPage'
+import { AllMdxQuery, MdxNode } from 'types/mdx-types'
+import getTitlePrefix from 'utils/getTitlePrefix'
+import SEO from './seo'
+
+export type HeadAPI<DataType = object, PageContextType = object> = (
+  props: HeadProps<DataType, PageContextType>,
+) => JSX.Element
+
+export const ArticlePageHead: HeadAPI<{ mdx: MdxNode }> = ({
+  location: { pathname },
+  data,
+}) => {
+  const title = data.mdx.frontmatter.title
+  return <SEO titlePrefix={title} canonical={pathname}></SEO>
+}
+
+export const CategoryPageHead: HeadAPI<AllMdxQuery, CategoryPageContext> = ({
+  location: { pathname },
+  pageContext: { categoryDirectory },
+}) => {
+  const titlePrefix = getTitlePrefix(
+    CategoryStrings.initialize(categoryDirectory),
+  )
+
+  return <SEO titlePrefix={titlePrefix} canonical={pathname}></SEO>
+}
