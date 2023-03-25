5231d9463469ea769985fbe4b752dcc4dc7adbc9
LeoBang <bluke8489@gmail.com>
2023-03-25 19:59:22 +0900

    [Chore] article page의 url endpoint 변경 (#45) - 이제 더이상 uri endpoint에 category를 나타내지 않음. - Head API를 이용해 title로 category를 나타내도록 함.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
 import Layout from 'components/Layout/index'
-import { HeadFC, Link } from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
-import { getStaticPageHead } from 'utils/Head'
-import {
-  INDEX_PAGE_DESCRIPTION,
-  INDEX_PAGE_TITLE,
-} from '../constants/PageConsts'
 
 const IndexPage = () => {
   return (
     <Layout>
-      IndexPage
-      <Link to="/about">about page로</Link>
+      <Link to="/categories">All Posts</Link>
     </Layout>
   )
 }
 
 export default IndexPage
-
-export const Head: HeadFC = getStaticPageHead({
-  title: INDEX_PAGE_TITLE,
-  description: INDEX_PAGE_DESCRIPTION,
-})
