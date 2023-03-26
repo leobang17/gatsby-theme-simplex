3a282e0eac9b08de91ec31d684ea4b6d68844144
LeoBang <bluke8489@gmail.com>
2023-03-26 14:22:16 +0900

    [Feat] slugify 함수 개선 (#26) - 연결성 character들을 hyphen으로 변경 (\s, _, :, ;) - camel case를 hyphen 기반 snake case로 변경 - url에 적합하지 않는 character들 제거 - 연속되는 hyphen들은 하나로 압축

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -1,5 +1,32 @@
 export function slugify(value: string) {
-  value = value.trimStart()
-  value = value.trimEnd()
-  return value.replace(/\s+/g, '-').toLowerCase()
+  let slug = trimSpaces(value)
+  slug = convertConnectiveCharToHyphen(slug)
+  slug = camelToHyphenedSnake(slug)
+  slug = removeUrlUnsafeChars(slug)
+  slug = condensConsecutiveHyphens(slug)
+  return slug
+}
+
+function trimSpaces(slug: string) {
+  return slug.trimStart().trimEnd()
+}
+
+function convertConnectiveCharToHyphen(slug: string) {
+  return slug.replace(/[\s_:;]+/g, '-')
+}
+
+function camelToHyphenedSnake(slug: string) {
+  return slug
+    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
+    .replace(/([a-z\d])([A-Z])/g, '$1_$2')
+    .replace(/[-\s]+/g, '-')
+    .toLowerCase()
+}
+
+function removeUrlUnsafeChars(slug: string) {
+  return slug.replace(/[.,!?:;"'`^*(){}]/g, '')
+}
+
+function condensConsecutiveHyphens(slug: string) {
+  return slug.replace(/-+/g, '-')
 }
