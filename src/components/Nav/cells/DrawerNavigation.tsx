40b2ca3183cea9174b3cb433759d337493d01d38
LeoBang <bluke8489@gmail.com>
2023-04-01 21:27:37 +0900

    [Feat] 모바일 용 nav 바 생성 (#33) - tablet S 사이즈 -> 우측에 drawer 토글버튼 생성

---
+++ src/components/Nav/cells/DrawerNavigation.tsx
@@ -0,0 +1,75 @@
+/** @jsx jsx */
+
+import { FC, useState } from 'react'
+import { jsx, css } from '@emotion/react'
+import { s_mq } from 'styles/facepaint'
+import { Box, IconButton, SwipeableDrawer, Typography } from '@mui/material'
+import MenuIcon from '@mui/icons-material/MenuRounded'
+import CloseIcon from '@mui/icons-material/CloseRounded'
+import CategoryTable from 'components/CategoryTable'
+import { SECOND } from 'styles/Color'
+
+type DrawerNavigationProps = {}
+
+const style = css(
+  css`
+    position: absolute;
+    right: 1rem;
+    align-items: center;
+  `,
+  s_mq({
+    display: ['flex', 'none'],
+  }),
+)
+
+const DrawerNavigation: FC<DrawerNavigationProps> = ({}) => {
+  const [drawerState, setDrawerState] = useState<boolean>(false)
+
+  return (
+    <nav css={style}>
+      <IconButton
+        size="small"
+        edge="start"
+        color="inherit"
+        aria-label="logo"
+        onClick={() => setDrawerState(true)}
+      >
+        <MenuIcon fontSize="small" />
+      </IconButton>
+      <SwipeableDrawer
+        anchor="right"
+        variant="temporary"
+        open={drawerState}
+        onClose={() => setDrawerState(false)}
+        onOpen={() => setDrawerState(true)}
+      >
+        <Box p={2} width="220px" role="presentation">
+          <IconButton
+            onClick={() => setDrawerState(false)}
+            size="small"
+            edge="start"
+          >
+            <CloseIcon
+              css={css`
+                margin-bottom: 10px;
+              `}
+              fontSize="small"
+            />
+          </IconButton>
+          <Typography fontWeight="bold" margin="0 0 10px 10px" color={SECOND}>
+            카테고리
+          </Typography>
+          <div
+            style={{
+              marginLeft: '10px',
+            }}
+          >
+            <CategoryTable />
+          </div>
+        </Box>
+      </SwipeableDrawer>
+    </nav>
+  )
+}
+
+export default DrawerNavigation
