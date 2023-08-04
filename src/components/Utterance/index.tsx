e6fe1175353286b9ee9b461db260eb85f379863f
LeoBang <bluke8489@gmail.com>
2023-08-04 20:51:13 +0900

    [Feat] utterance 컴포넌트에 theme 적용 - 계속 DOM에 child를 append하는 로직도 수정

--- src/components/Utterance/index.tsx
+++ src/components/Utterance/index.tsx
@@ -1,3 +1,4 @@
+import { ThemeContext } from 'contexts/theme/ThemeContext'
 import MetadataApiConfigurator from 'datalayer/configurators/MetadataApiConfigurator'
 import React, { createRef, useLayoutEffect } from 'react'
 
@@ -7,15 +8,18 @@ export interface UtterancesProps {}
 
 const Utterances: React.FC<UtterancesProps> = React.memo(() => {
   const { issue_repo } = MetadataApiConfigurator.instance.api.getUtterance()
+  const { mode } = React.useContext(ThemeContext)
+
   const containerRef = createRef<HTMLDivElement>()
 
   useLayoutEffect(() => {
     const utterances = document.createElement('script')
+    const containerDiv = containerRef.current!
 
     const attributes = {
       src,
       repo: issue_repo.value,
-      theme: 'github-light',
+      theme: mode === 'dark' ? 'github-dark' : 'github-light',
       'issue-term': 'pathname',
       label: '✨💬 comments ✨',
       crossOrigin: 'anonymous',
@@ -26,7 +30,11 @@ const Utterances: React.FC<UtterancesProps> = React.memo(() => {
       utterances.setAttribute(key, value)
     })
 
-    containerRef.current!.appendChild(utterances)
+    while (containerDiv.lastChild) {
+      containerDiv.removeChild(containerDiv.lastChild)
+    }
+
+    containerDiv.appendChild(utterances)
   }, [issue_repo])
 
   return <div ref={containerRef} />
