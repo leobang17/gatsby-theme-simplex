ebd1311b2a09863e4e245f3c8a2a4db227372e17
LeoBang <bluke8489@gmail.com>
2023-08-19 12:00:46 +0900

    [chore] prism-react-render로 변경에 따른 configuration 변경

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,22 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-
-// import 'prism-themes/themes/prism-coldark-cold.css'
-// import 'prism-themes/themes/prism-base16-ateliersulphurpool.light.css'
-// import 'prism-themes/themes/prism-duotone-sea.css'
-// import 'prism-themes/themes/prism-duotone-light.css'
-// import 'prism-themes/themes/prism-material-dark.css'
-// import 'prism-themes/themes/prism-material-light.css'
-// import 'prism-themes/themes/prism-nord.css'
-import 'prism-themes/themes/prism-one-light.css'
-// import 'prism-themes/themes/prism-one-dark.css'
-// import 'prism-themes/themes/prism-synthwave84.css'
-// import 'prism-themes/themes/prism-vs.css'
-// import 'prism-themes/themes/prism-vsc-dark-plus.css'
-// import 'prism-themes/themes/prism-vsc-dark-plus.css'
-
-// import 'prismjs/plugins/line-numbers/prism-line-numbers.css' // De-comment this if you want to mark lines for code block.
+// import './language-tabs.css'
 import './src/styles/global.css'
 
 import '@fontsource/noto-sans'
